830e2be856cb4b3f7cd3dfc573eddf552e372aeb
Egor I <egor.ivanov@pm.me>
2026-08-14 12:42:52 +0200

    sigue estos pasos
    Signed-off-by: Egor I <egor.ivanov@pm.me>

--- tests/unit_3k/test_always_passing_assert_003.py
+++ tests/unit_3k/test_always_passing_assert_003.py
@@ -11,6 +11,10 @@ def test_unit_always_passing_000():
         assert not _should_fail(), "Failure due to reason ODD"
     with allure.step("Assert 123 versus 123"):
         assert not _should_fail(), "Failure due to reason ODD"
+    with allure.step("Assert 123 versus 123"):
+        assert not _should_fail(), "Failure due to reason ODD"
+    with allure.step("Assert 123 versus 123"):
+        assert not _should_fail(), "Failure due to reason ODD"
 
 
 @allure.feature("test results processing")
