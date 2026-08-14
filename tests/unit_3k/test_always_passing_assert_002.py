e7f938a7bb822470e93f38de7f18788553e5423c
Egor I <egor.ivanov@pm.me>
2026-08-14 12:42:27 +0200

    una cerveza por favor
    Signed-off-by: Egor I <egor.ivanov@pm.me>

--- tests/unit_3k/test_always_passing_assert_002.py
+++ tests/unit_3k/test_always_passing_assert_002.py
@@ -21,8 +21,6 @@ def test_unit_always_passing_001():
         assert not _should_fail(), "Failure due to reason ODD"
     with allure.step("Assert 123 versus 123"):
         assert not _should_fail(), "Failure due to reason ODD"
-    with allure.step("Assert 123 versus 123"):
-        assert not _should_fail(), "Failure due to reason ODD"
 
 @allure.feature("test results processing")
 @allure.story("many unit tests")
